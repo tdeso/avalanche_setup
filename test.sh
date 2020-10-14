9d5a75bd9fb0601d2055b585fb8f703c3acf280c
tdeso <48097413+tdeso@users.noreply.github.com>
2020-10-14 13:41:04 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $USER == "root"]]; then
+if [[ $USER == "root" ]]; then
     apt-get -y update && apt-get -y upgrade
     apt-get install git -y
     git clone https://github.com/tdeso/avalanche_setup
