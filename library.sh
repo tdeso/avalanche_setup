06fb3209fedb0210f33dc4326554ea16174f5d53
tdeso <thibaud.desormiere@gmail.com>
2020-10-14 15:21:44 +0200

    sleep ajustment

--- library.sh
+++ library.sh
@@ -358,7 +358,7 @@ function launchAvalanche() {
   sudo systemctl enable avalanche
   sudo systemctl start avalanche
   NODE_STATUS=$(eval node_status)
-  sleep .5
+  sleep 1
   NODE_ID=$(eval node_ID)
 }
 
