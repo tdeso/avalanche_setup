e1f26eb55b07182eade63f18d086681359af8602
tdeso <thibaud.desormiere@gmail.com>
2020-10-15 16:23:55 +0200

    fix

--- test.sh
+++ test.sh
@@ -3,13 +3,13 @@
 if [[ $USER == "root" ]]; then
     apt-get -y update && apt-get -y upgrade
     apt-get install git -y
-    git clone https://github.com/tdeso/avalanche_setup
+    git clone https://github.com/tdeso/avalanche_setup.git
     cd avalanche_setup
     bash setup.sh
     cd
     rm -rf avalanche_setup
 else
-    git clone https://github.com/tdeso/avalanche_setup
+    git clone https://github.com/tdeso/avalanche_setup.git
     cd avalanche_setup
     sudo bash install.sh
     cd 
