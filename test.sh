4b04fc343e96162664c909b6f3b23e8d8ba15192
tdeso <thibaud.desormiere@gmail.com>
2020-10-15 16:26:25 +0200

    fix

--- test.sh
+++ test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [[ $USER == "root" ]]; then
-    apt-get -y update && apt-get -y upgrade
-    apt-get install git -y
-    git clone https://github.com/tdeso/avalanche_setup.git
-    cd avalanche_setup
-    bash setup.sh
-    cd
+    apt-get -y update && apt-get -y upgrade;
+    apt-get install git -y;
+    git clone https://github.com/tdeso/avalanche_setup.git;
+    cd avalanche_setup;
+    bash setup.sh;
+    cd;
     rm -rf avalanche_setup
 else
-    git clone https://github.com/tdeso/avalanche_setup.git
-    cd avalanche_setup
-    sudo bash install.sh
-    cd 
+    git clone https://github.com/tdeso/avalanche_setup.git;
+    cd avalanche_setup;
+    sudo bash install.sh;
+    cd;
     find $HOME/avalanche_setup ! '(' -name 'update.sh' -o -name 'monitor.sh' -o -name 'library.sh' ')' -delete 
 fi
