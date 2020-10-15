a4060bbe5de611416aa35b25eafdc208d06ad4af
tdeso <thibaud.desormiere@gmail.com>
2020-10-15 16:22:20 +0200

    fix

--- test.sh
+++ test.sh
@@ -11,5 +11,7 @@ if [[ $USER == "root" ]]; then
 else
     git clone https://github.com/tdeso/avalanche_setup
     cd avalanche_setup
-    sudo bash install.sh && cd && find $HOME/avalanche_setup ! '(' -name 'update.sh' -o -name 'monitor.sh' -o -name 'library.sh' ')' -delete 
+    sudo bash install.sh
+    cd 
+    #find $HOME/avalanche_setup ! '(' -name 'update.sh' -o -name 'monitor.sh' -o -name 'library.sh' ')' -delete 
 fi
