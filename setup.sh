b5f2e74a020dd74b4d4e4016cc6bad09cc70972b
tdeso <thibaud.desormiere@gmail.com>
2020-10-16 12:16:20 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -4,20 +4,18 @@
 # For more information, see: https://https://github.com/tdeso/avalanche_setup
 
 if [[ $USER == "root" ]]; then
-    apt-get -y update;
-    apt-get -y upgrade;
-    apt-get -y install git ;
-    cd ~;
-    git clone https://github.com/tdeso/avalanche_setup.git;
-    bash ~/avalanche_setup/node_setup.sh;
+    apt-get -y update
+    apt-get -y upgrade
+    apt-get -y install git
+    cd ~
+    git clone https://github.com/tdeso/avalanche_setup.git
+    bash ~/avalanche_setup/node_setup.sh
     rm -rf ~/avalanche_setup
-
     else
-
-    git clone https://github.com/tdeso/avalanche_setup.git;
-    sudo bash ~/avalanche_setup/node_install.sh;
-    cd $HOME/avalanche_setup/;
+    git clone https://github.com/tdeso/avalanche_setup.git
+    sudo bash ~/avalanche_setup/node_install.sh
+    cd $HOME/avalanche_setup/
     find . ! '(' -name 'update.sh' -o -name 'monitor.sh' -o -name 'library.sh' ')' -exec rm -rf {} +
     cd ~
-    rm -rf go1.13.linux-amd64.tar.gz;
+    rm -rf go1.13.linux-amd64.tar.gz
 fi
